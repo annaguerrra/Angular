d56a4e741efe82cafacc7dd3e6ef4924f0b2be3c
annaguerrra <annaguerracastro@gmail.com>
2026-03-26 16:51:22 -0300

    main page

--- aula03X/src/app/app-routing.module.ts
+++ aula03X/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './features/login-page/login-page.component';
+import { MainPageComponent } from './features/main-page/main-page.component';
 
 const routes: Routes = [
-  { path: "login", component: LoginPageComponent}
+  { path: "", component: MainPageComponent, children: [
+    {path: "login", component: LoginPageComponent }
+  ]}
 ];
 
 @NgModule({
--- aula03X/src/app/features/login-page/login-page.component.ts
+++ aula03X/src/app/features/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+  isSubscribe = false;
 
+  constructor(
+    private _router: Router,
+    private _activatedRouter: ActivatedRoute
+  ){
+    // serviços de injeção de dependência
+  }
+
+  ngOnInit(): void {
+    this._activatedRouter.params.subscribe((params) => {
+      this.isSubscribe = params['tab'] == "subscribe";
+    })
+  }
 }
